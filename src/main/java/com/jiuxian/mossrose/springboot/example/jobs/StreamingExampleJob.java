408e1ba4186782fec457765c08cf982acba10acd
yuxuanwang <crnlmcn@gmail.com>
2018-05-17 10:09:22 +0800

    update version

--- src/main/java/com/jiuxian/mossrose/springboot/example/jobs/StreamingExampleJob.java
+++ src/main/java/com/jiuxian/mossrose/springboot/example/jobs/StreamingExampleJob.java
@@ -3,33 +3,35 @@ package com.jiuxian.mossrose.springboot.example.jobs;
 import com.google.common.collect.Lists;
 import com.jiuxian.boot.mossrose.autoconfigure.Job;
 import com.jiuxian.mossrose.job.StreamingJob;
+import com.jiuxian.mossrose.util.Tuple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @Job(id = "StreamingExampleJob", cron = "0 * * * * ?", group = "example")
-public class StreamingExampleJob implements StreamingJob<String> {
+public class StreamingExampleJob implements StreamingJob<String, Integer> {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(StreamingExampleJob.class);
 
-	@Override
-	public Streamer<String> streamer() {
-		return new Streamer<String>() {
+	private static final List<String> LIST = Lists.newArrayList("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 
-			private List<String> list = Lists.newArrayList("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+	@Override
+	public Streamer<String, Integer> streamer() {
+		return new Streamer<String, Integer>() {
+
+            @Override
+            public Tuple<String, Integer> next(Integer mark) {
+                int index = mark != null ? mark + 1 : 0;
+                if(index > LIST.size() - 1) {
+                    return null;
+                }
+
+                return new Tuple(LIST.get(index), index);
+            }
 
 			private int index = 0;
 
-			@Override
-			public boolean hasNext() {
-				return index < list.size();
-			}
-
-			@Override
-			public String next() {
-				return list.get(index++);
-			}
 		};
 	}
 
